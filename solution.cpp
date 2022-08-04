bcb36b89924de0274130c4eff466a44f710746f5
Ajay Dandge <60701544+er-knight@users.noreply.github.com>
2022-08-04 18:19:42 +0530

    add files

---
+++ solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+
+}
+
+int main() {
+    long long t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ solution.py
@@ -0,0 +1,9 @@
+def solve() -> None:
+    pass
+
+if __name__ == "__main__":
+
+    t = 1
+    # t = int(input())
+    for _ in range(t):
+        solve()
